b464b3244081bfe342ba79cb8e33dd6610fae7a3
IlyPoh <pohnv888888@gmail.com>
2023-03-05 15:35:30 +0200

    small refactoring (moving forEach, changing let into const)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,58 +2,60 @@ import { Validator } from './components/validator.js';
 import { loginFormConfig } from './components/form-config.js';
 import { setErrorState, setSuccessState, setNeutralState, changeHTML } from './components/helpers.js'
 
-let forms = document.querySelectorAll('form');
+const forms = document.querySelectorAll('form');
+
+const handleFormSubmit = (form) => (e) => {
+    e.preventDefault();
+
+    const elements = form.elements;
+        
+    [...elements].forEach( element => {
+        if(element.type !== 'submit') {
+            const errorBox = form.querySelector(`[data-for="${element.name}"]`);
+
+            setNeutralState(element)
+            changeHTML(errorBox, '')
+        }
+    })
+    
+    const isValid = Validator.validate(form, loginFormConfig);
+    
+    if(!isValid) {
+        const errors = Validator.getErrors(form.name);
+
+        Object.entries(errors).forEach(([name, errorObject]) => {
+            const errorBox = form.querySelector(`[data-for="${name}"]`);
+            setErrorState(elements[name], errorBox)
+
+            const errorMessage = Object.values(errorObject).map( message => `<p>${message}</p>`).join('');
+            changeHTML(errorBox, errorMessage)
+        });
+    };
+}
+
+const handleFormInput = (form) => (e) => {
+    const target = e.target;
+    const errorBox = form.querySelector(`[data-for="${target.name}"]`);
+    
+    const isValid = Validator.validate(
+        form, 
+        { [target.name]: loginFormConfig[target.name] },
+    );
+
+    if(!isValid) {
+        const errors = Validator.getErrors(form.name)?.[target.name];
+        const errorMessage = Object.values(errors).map( message => `<p>${message}</p>`).join('');
+        setErrorState(target, errorBox)
+        changeHTML(errorBox, errorMessage)
+
+        return;
+    }
+
+    setSuccessState(target);
+    changeHTML(errorBox, '')
+}
 
 forms.forEach((form) => {
-    let elements = form.elements;
-    
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        
-        [...elements].forEach( element => {
-            if(element.type !== 'submit') {
-                let errorBox = form.querySelector(`[data-for="${element.name}"]`);
-    
-                setNeutralState(element)
-                changeHTML(errorBox, '')
-            }
-        })
-    
-        let isValid = Validator.validate(form, loginFormConfig);
-    
-        if(!isValid) {
-            let errors = Validator.getErrors(form.name);
-    
-            Object.entries(errors).forEach(([name, errorObject]) => {
-                let errorBox = form.querySelector(`[data-for="${name}"]`);
-                setErrorState(elements[name], errorBox)
-    
-                let errorMessage = Object.values(errorObject).map( message => `<p>${message}</p>`).join('');
-                changeHTML(errorBox, errorMessage)
-            });
-        };
-    });
-    
-    
-    form.addEventListener('input', (e) => {
-        let target = e.target;
-        let errorBox = form.querySelector(`[data-for="${target.name}"]`);
-    
-        let isValid = Validator.validate(
-            form, 
-            { [target.name]: loginFormConfig[target.name] },
-        );
-    
-        if(!isValid) {
-            let errors = Validator.getErrors(form.name)?.[target.name];
-            let errorMessage = Object.values(errors).map( message => `<p>${message}</p>`).join('');
-            setErrorState(target, errorBox)
-            changeHTML(errorBox, errorMessage)
-    
-            return;
-        }
-    
-        setSuccessState(target);
-        changeHTML(errorBox, '')
-    })
+    form.addEventListener('submit', handleFormSubmit(form));
+    form.addEventListener('input', handleFormInput(form));
 })
